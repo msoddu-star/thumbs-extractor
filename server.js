642c7c10dc18813884c82340fbbf04bce9aba56c
msoddu-star <m.soddu@merlinwizard.com>
2026-05-05 15:42:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 app.use(express.static('public'));
 
-app.get('/app', function(req, res) {
-    res.send('App is running!');
+app.get('/app', (req, res) => {
+  res.sendFile(__dirname + '/public/app.html');
 });
 
 // Existing middleware and other routes below this line
